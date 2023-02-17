81239adda2ec958b52fcac5d87c3e3bb9fede121
PrinceObilor <ugonnaprince97@gmail.com>
2023-02-16 18:49:30 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - print line
  *
- * Return: o when successful
+ * Return: 0 when successful
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
